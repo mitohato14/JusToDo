b3e29b42d397e1305ddd80e0b9eac089fa7481e8
mitohato <mitohato14@gmail.com>
2018-06-27 21:57:18 +0900

    Devide line

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.ict.mito.justodo.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.ict.mito.justodo.R
 
 class MainFragment : Fragment() {
@@ -21,7 +21,11 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(
+                R.layout.main_fragment,
+                container,
+                false
+        )
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
