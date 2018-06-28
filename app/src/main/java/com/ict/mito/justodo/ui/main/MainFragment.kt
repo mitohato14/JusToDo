64f526befc0cdbb1a06d9bfaafc87b7be9e6013b
mitohato <mitohato14@gmail.com>
2018-06-28 11:31:23 +0900

    Remove ViewModel

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.ict.mito.justodo.R
 
 class MainFragment : Fragment() {
@@ -26,11 +25,5 @@ class MainFragment : Fragment() {
                 container,
                 false
         )
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 }
