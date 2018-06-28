bdcb3630cbd2b958cba5d35b84690c3f7055b298
mitohato <mitohato14@gmail.com>
2018-06-28 15:09:18 +0900

    Remove ViewModel

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
@@ -13,8 +13,6 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
