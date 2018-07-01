2c9345f6c2b5b370c8e8ac2fca395e80ecedaaf1
mitohato <mitohato14@gmail.com>
2018-07-02 07:33:44 +0900

    Add subscribe method

--- app/src/main/java/com/ict/mito/justodo/ToDoStore.kt
+++ app/src/main/java/com/ict/mito/justodo/ToDoStore.kt
@@ -1,5 +1,6 @@
 package com.ict.mito.justodo
 
+import me.retty.reduxkt.Action
 import me.retty.reduxkt.Store
 
 object ToDoStore {
@@ -20,4 +21,15 @@ object ToDoStore {
 
     fun subscribe(subscriber: Subscriber) = this.store.subscribe(subscriber)
     fun unsubscribe(subscriber: Subscriber) = this.store.unsubscribe(subscriber)
+
+    fun dispatch(action: Action) = this.store.dispatch(action)
+    fun dispatch(actionCreator: ActionCreator) = this.store.dispatch(actionCreator)
+    fun dispatch(actionCreator: AsyncActionCreator) = this.store.dispatch(actionCreator)
+    fun dispatch(
+        actionCreator: AsyncActionCreator,
+        callback: (ToDoState) -> Unit
+    ) = this.store.dispatch(
+            actionCreator,
+            callback
+    )
 }
