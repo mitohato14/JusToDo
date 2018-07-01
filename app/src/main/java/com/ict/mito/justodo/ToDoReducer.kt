01c03c0f4aa67220b899352ecdc5b3a6a848d240
mitohato <mitohato14@gmail.com>
2018-07-01 22:02:46 +0900

    Rename

--- app/src/main/java/com/ict/mito/justodo/ToDoReducer.kt
+++
@@ -1,3 +0,0 @@
-package com.ict.mito.justodo
-
-class ToDoReducer
---
+++ app/src/main/java/com/ict/mito/justodo/ToDoReducerSet.kt
@@ -0,0 +1,9 @@
+package com.ict.mito.justodo
+
+class ToDoReducerSet {
+    
+    companion object {
+        fun aggregatedReducer(action: ToDoActicion, state: ToDoState) = state
+    }
+    
+}
