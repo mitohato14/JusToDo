6e7da871378582f33b1d9240b903149167259ede
mitohato <mitohato14@gmail.com>
2018-07-01 22:02:56 +0900

    Add RootReducerSet

---
+++ app/src/main/java/com/ict/mito/justodo/RootReducerSet.kt
@@ -0,0 +1,16 @@
+package com.ict.mito.justodo
+
+import me.retty.reduxkt.Reducer
+
+class RootReducerSet {
+    companion object {
+        val aggregatedReducer: Reducer<ToDoState> = { action, state ->
+            when(action) {
+            
+            
+            
+            }
+        
+        }
+    }
+}
