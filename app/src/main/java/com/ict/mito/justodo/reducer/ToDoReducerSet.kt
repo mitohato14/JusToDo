06e60725c54a4bee48038fb9b4e111a54bfb5790
mitohato <mitohato14@gmail.com>
2018-07-02 21:58:14 +0900

    Implement ToDoReducer

--- app/src/main/java/com/ict/mito/justodo/reducer/ToDoReducerSet.kt
+++ app/src/main/java/com/ict/mito/justodo/reducer/ToDoReducerSet.kt
@@ -6,6 +6,35 @@ import com.ict.mito.justodo.state.ToDoState
 class ToDoReducerSet {
 
     companion object {
-        fun aggregatedReducer(action: ToDoAction, state: ToDoState) = state
+        fun aggregatedReducer(action: ToDoAction, state: ToDoState) = when (action) {
+            is ToDoAction.OnCreateToDoAction -> onCreateToDoAction(
+                    action,
+                    state
+            )
+            is ToDoAction.OnEditToDoAction -> onEditToDoAction(
+                    action,
+                    state
+            )
+            is ToDoAction.onUpdateToDoAction -> onUpdateToDoAction(
+                    action,
+                    state
+            )
+            else -> state
+        }
+
+        private fun onCreateToDoAction(
+            action: ToDoAction.OnCreateToDoAction,
+            state: ToDoState
+        ) = state.copy(todoInfoList = state.todoInfoList + action.toDoInfo)
+
+        private fun onEditToDoAction(
+            action: ToDoAction.OnEditToDoAction,
+            state: ToDoState
+        ) = state
+
+        private fun onUpdateToDoAction(
+            action: ToDoAction.onUpdateToDoAction,
+            state: ToDoState
+        ) = state // TODO:ここでToDo編集後に情報が更新されたリストを渡す
     }
 }
