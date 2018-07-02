764001b7adbe6b0fb52602aa1e550f8aa62dc5eb
mitohato <mitohato14@gmail.com>
2018-07-02 12:13:02 +0900

    Add action

--- app/src/main/java/com/ict/mito/justodo/action/ToDoAction.kt
+++ app/src/main/java/com/ict/mito/justodo/action/ToDoAction.kt
@@ -1,3 +1,16 @@
 package com.ict.mito.justodo.action
 
-interface ToDoAction
+import com.ict.mito.justodo.model.ToDoInfo
+import me.retty.reduxkt.Action
+
+interface ToDoAction : Action {
+
+    class OnCreateToDoAction(val toDoInfo: ToDoInfo) : ToDoAction
+
+    class OnEditToDoAction(val taskId: String) : ToDoAction
+
+    class onUpdateToDoAction(
+        val taskId: String,
+        val toDoInfo: ToDoInfo
+    )
+}
