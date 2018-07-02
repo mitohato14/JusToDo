375aec7e1db5efe48bac463c31e30049381e89fc
mitohato <mitohato14@gmail.com>
2018-07-02 21:30:35 +0900

    Rename description

--- app/src/main/java/com/ict/mito/justodo/model/ToDoInfo.kt
+++ app/src/main/java/com/ict/mito/justodo/model/ToDoInfo.kt
@@ -3,7 +3,7 @@ package com.ict.mito.justodo.model
 data class ToDoInfo(
     val id: String,
     val title: String,
-    val content: String,
+    val description: String,
     val dueData: Int,
     val deadline: Int
 )
