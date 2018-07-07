defd82885a0c284a542f9827930514959110b039
mitohato <mitohato14@gmail.com>
2018-07-07 22:14:41 +0900

    Add call Fragment

--- app/src/main/java/com/ict/mito/justodo/ui/add/AddActivity.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/add/AddActivity.kt
@@ -9,5 +9,13 @@ class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(
+                            R.id.container,
+                            AddFragment.newInstance()
+                    )
+                    .commitNow()
+        }
     }
 }
