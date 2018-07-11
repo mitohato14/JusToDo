5e1ae63c09ffb24ed96c813f1afe84c88c930e35
mitohato <mitohato14@gmail.com>
2018-07-11 11:51:03 +0900

    Implement actionCreator

--- app/src/main/java/com/ict/mito/justodo/action/ToDoActionCreatorProducer.kt
+++ app/src/main/java/com/ict/mito/justodo/action/ToDoActionCreatorProducer.kt
@@ -1,3 +1,35 @@
 package com.ict.mito.justodo.action
 
-class ToDoActionCreatorProducer
+import com.ict.mito.justodo.AsyncActionCreator
+import com.ict.mito.justodo.model.ToDoInfo
+
+class ToDoActionCreatorProducer {
+
+    companion object {
+        fun produceCreateToDoAction(toDoInfo: ToDoInfo): AsyncActionCreator = { _, _, callback ->
+            callback { _, _ ->
+                ToDoAction.OnCreateToDoAction(toDoInfo)
+            }
+        }
+
+        fun produceEditToDoAction(taskId: String): AsyncActionCreator = { _, _, callback ->
+            callback { _, _ ->
+                ToDoAction.OnEditToDoAction(taskId)
+            }
+        }
+
+        fun produceUpdateToDoAction(
+            taskId: String,
+            index: Int,
+            toDoInfo: ToDoInfo
+        ): AsyncActionCreator = { _, _, callback ->
+            callback { _, _ ->
+                ToDoAction.OnUpdateToDoAction(
+                        taskId,
+                        index,
+                        toDoInfo
+                )
+            }
+        }
+    }
+}
