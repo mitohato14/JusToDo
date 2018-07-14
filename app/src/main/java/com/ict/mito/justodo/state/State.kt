72e4d471d77705d379372b36fc87e40906eedf37
mitohato <mitohato14@gmail.com>
2018-07-14 22:43:52 +0900

    Add state

---
+++ app/src/main/java/com/ict/mito/justodo/state/State.kt
@@ -0,0 +1,8 @@
+package com.ict.mito.justodo.state
+
+import com.ict.mito.justodo.model.ToDoInfo
+
+/**
+ * Created by mito on 2018/07/14.
+ */
+data class State(val toDoInfoList: List<ToDoInfo> = emptyList())
