707539edd392098044599de968926e650957aaef
mitohato <mitohato14@gmail.com>
2018-07-16 06:51:32 +0900

    Add Actions

---
+++ app/src/main/java/com/ict/mito/justodo/action/Actions.kt
@@ -0,0 +1,11 @@
+package com.ict.mito.justodo.action
+
+import info.izumin.android.droidux.Action
+
+/**
+ * Created by mito on 2018/07/16.
+ */
+class CreateToDoInfo() : Action
+class EditToDoInfo() : Action
+class UpdateToDoInfo() : Action
+class DeleteToDoInfo() : Action
