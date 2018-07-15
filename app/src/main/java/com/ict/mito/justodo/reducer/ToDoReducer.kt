877ccb44d994664fd7ea093f95958b48f559b045
mitohato <mitohato14@gmail.com>
2018-07-16 06:29:00 +0900

    Add ToDoReducer class

---
+++ app/src/main/java/com/ict/mito/justodo/reducer/ToDoReducer.kt
@@ -0,0 +1,11 @@
+package com.ict.mito.justodo.reducer
+
+import com.ict.mito.justodo.state.State
+import info.izumin.android.droidux.annotation.Reducer
+
+/**
+ * Created by mito on 2018/07/16.
+ */
+@Reducer(State::class)
+class ToDoReducer {
+}
