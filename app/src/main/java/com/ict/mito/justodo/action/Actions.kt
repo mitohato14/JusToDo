a00761fc1f0d10697662e066b9938b11b7e57311
mitohato <mitohato14@gmail.com>
2018-07-16 07:30:55 +0900

    Add args

--- app/src/main/java/com/ict/mito/justodo/action/Actions.kt
+++ app/src/main/java/com/ict/mito/justodo/action/Actions.kt
@@ -1,11 +1,15 @@
 package com.ict.mito.justodo.action
 
+import com.ict.mito.justodo.model.ToDoInfo
 import info.izumin.android.droidux.Action
 
 /**
  * Created by mito on 2018/07/16.
  */
-class CreateToDoInfo() : Action
-class EditToDoInfo() : Action
-class UpdateToDoInfo() : Action
-class DeleteToDoInfo() : Action
+class CreateToDoInfo(toDoInfo: ToDoInfo) : Action
+class EditToDoInfo(taskId: String) : Action
+class UpdateToDoInfo(
+        taskId: String,
+        toDoInfo: ToDoInfo
+) : Action
+class DeleteToDoInfo(taskId: String) : Action
