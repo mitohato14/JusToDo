abc5a2e0f4781dffbd6b00e20843337feb1bc0bc
mitohato <mitohato14@gmail.com>
2018-07-16 13:05:36 +0900

    Add getToDoById method

--- app/src/main/java/com/ict/mito/justodo/state/ToDoState.kt
+++ app/src/main/java/com/ict/mito/justodo/state/ToDoState.kt
@@ -1,8 +1,16 @@
 package com.ict.mito.justodo.state
 
 import com.ict.mito.justodo.model.ToDoInfo
+import io.reactivex.Observable
 
 /**
  * Created by mito on 2018/07/14.
  */
-data class ToDoState(val toDoInfoList: List<ToDoInfo> = emptyList())
+data class ToDoState(val toDoInfoList: List<ToDoInfo> = emptyList()) {
+
+    fun getToDoById(id: String) = Observable
+            .fromIterable(toDoInfoList)
+            .filter { id == it.id }
+            .blockingFirst()
+
+}
