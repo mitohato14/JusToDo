5154b45984c4efbc524cbe69eee9ff78cb3e360a
mitohato <mitohato14@gmail.com>
2018-07-16 15:05:54 +0900

    Extend BaseStore

--- app/src/main/java/com/ict/mito/justodo/store/ToDoStore.kt
+++ app/src/main/java/com/ict/mito/justodo/store/ToDoStore.kt
@@ -1,12 +1,13 @@
 package com.ict.mito.justodo.store
 
 import com.ict.mito.justodo.state.ToDoState
+import info.izumin.android.droidux.BaseStore
 import io.reactivex.Flowable
 
 /**
  * Created by mito on 2018/07/16.
  */
-interface ToDoStore {
+interface ToDoStore : BaseStore{
     fun getState(): ToDoState
     fun observeState(): Flowable<ToDoState>
 }
