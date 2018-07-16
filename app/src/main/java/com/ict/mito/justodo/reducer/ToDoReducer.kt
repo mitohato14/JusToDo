561820fb2ee9d1652ef4ebf11cf61ba9bfc6dde2
mitohato <mitohato14@gmail.com>
2018-07-16 09:01:52 +0900

    Add reducer

--- app/src/main/java/com/ict/mito/justodo/reducer/ToDoReducer.kt
+++ app/src/main/java/com/ict/mito/justodo/reducer/ToDoReducer.kt
@@ -1,6 +1,10 @@
 package com.ict.mito.justodo.reducer
 
+import com.ict.mito.justodo.action.CreateToDoInfo
+import com.ict.mito.justodo.action.DeleteToDoInfo
+import com.ict.mito.justodo.model.ToDoInfo
 import com.ict.mito.justodo.state.State
+import info.izumin.android.droidux.annotation.Dispatchable
 import info.izumin.android.droidux.annotation.Reducer
 
 /**
@@ -8,4 +12,16 @@ import info.izumin.android.droidux.annotation.Reducer
  */
 @Reducer(State::class)
 class ToDoReducer {
+
+    @Dispatchable(CreateToDoInfo::class)
+    fun create(
+        state: State,
+        toDoInfo: ToDoInfo
+    ): State = State(state.toDoInfoList + toDoInfo)
+
+    @Dispatchable(DeleteToDoInfo::class)
+    fun delete(
+        state: State,
+        toDoInfo: ToDoInfo
+    ): State = State(state.toDoInfoList - toDoInfo)
 }
