c19106821c8a9370edd5710d06d5b6135407374e
mitohato <mitohato14@gmail.com>
2018-07-27 06:09:45 +0900

    Add store anotation

--- app/src/main/java/com/ict/mito/justodo/store/ToDoStore.kt
+++ app/src/main/java/com/ict/mito/justodo/store/ToDoStore.kt
@@ -1,12 +1,15 @@
-package com.ict.mito.justodo.store
+package com.ict.mito.justodo.module
 
+import com.ict.mito.justodo.reducer.ToDoReducer
 import com.ict.mito.justodo.state.ToDoState
 import info.izumin.android.droidux.BaseStore
+import info.izumin.android.droidux.annotation.Store
 import io.reactivex.Flowable
 
 /**
  * Created by mito on 2018/07/16.
  */
+@Store(ToDoReducer::class)
 interface ToDoStore : BaseStore {
     fun getState(): ToDoState
     fun observeState(): Flowable<ToDoState>
