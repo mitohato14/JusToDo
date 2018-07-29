68e49219a85585f7f4953a975df2b136d41b9a47
mitohato <mitohato14@gmail.com>
2018-07-29 20:42:16 +0900

    Extends ReduxAction

--- app/src/main/java/com/ict/mito/justodo/action/Action.kt
+++ app/src/main/java/com/ict/mito/justodo/action/Action.kt
@@ -1,12 +1,14 @@
 package com.ict.mito.justodo.action
 
+import com.github.rozag.redux.core.ReduxAction
+
 /**
  * Created by mito on 2018/07/14.
  */
-sealed class Action {
+sealed class Action : ReduxAction {
     class AddToDo : Action()
     class EditToDo : Action()
     class DeleteToDo : Action()
-    class CompleteToDo: Action()
+    class CompleteToDo : Action()
     class UnCompleteToDo : Action()
 }
