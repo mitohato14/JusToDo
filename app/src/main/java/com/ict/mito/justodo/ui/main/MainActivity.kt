fefb488449346646a20a7dbf019833584a43bc47
mitohato <mitohato14@gmail.com>
2018-07-29 21:38:53 +0900

    Add store companion object to MainActivity

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainActivity.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainActivity.kt
@@ -2,9 +2,20 @@ package com.ict.mito.justodo.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.rozag.redux.base.SubscribableStore
 import com.ict.mito.justodo.R
+import com.ict.mito.justodo.ToDoStore
+import com.ict.mito.justodo.action.ToDoAction
+import com.ict.mito.justodo.reducer.rootReducer
+import com.ict.mito.justodo.state.ToDoState
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val store: ToDoStore = SubscribableStore(
+                ToDoState.INITIAL,
+                ::rootReducer
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +28,6 @@ class MainActivity : AppCompatActivity() {
                     )
                     .commitNow()
         }
+        store.dispatch(ToDoAction.InitToDo())
     }
 }
