204bcf68ea22c68d538d8dcdecfad03500801d65
mitohato <mitohato14@gmail.com>
2018-07-29 18:30:45 +0900

    Change Action class to Sealed class

--- app/src/main/java/com/ict/mito/justodo/action/Action.kt
+++ app/src/main/java/com/ict/mito/justodo/action/Action.kt
@@ -3,9 +3,10 @@ package com.ict.mito.justodo.action
 /**
  * Created by mito on 2018/07/14.
  */
-enum class Action {
-    AddToDo,
-    EditToDo,
-    DeleteToDo,
-    CompleteToDo
+sealed class Action {
+    class AddToDo : Action()
+    class EditToDo : Action()
+    class DeleteToDo : Action()
+    class CompleteToDo: Action()
+    class UnCompleteToDo : Action()
 }
