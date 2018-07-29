c5d67db395a509dd9c689cfecdc40063fd660bcf
mitohato <mitohato14@gmail.com>
2018-07-29 20:45:18 +0900

    Add INITIAL companion object

--- app/src/main/java/com/ict/mito/justodo/state/ToDoState.kt
+++ app/src/main/java/com/ict/mito/justodo/state/ToDoState.kt
@@ -7,7 +7,11 @@ import io.reactivex.Observable
 /**
  * Created by mito on 2018/07/14.
  */
-data class ToDoState(val toDoInfoList: List<ToDoInfo> = emptyList()) : ReduxState {
+data class ToDoState(val toDoInfoList: List<ToDoInfo>) : ReduxState {
+
+    companion object {
+        val INITIAL: ToDoState = ToDoState(emptyList())
+    }
 
     fun getToDoById(id: String): ToDoInfo = Observable
             .fromIterable(toDoInfoList)
