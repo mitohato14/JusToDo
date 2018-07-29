218d3fbb2231110d5ecc97fbed851ea9c956697e
mitohato <mitohato14@gmail.com>
2018-07-30 06:22:38 +0900

    Add store to MainActivity

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainActivity.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainActivity.kt
@@ -2,9 +2,19 @@ package com.ict.mito.justodo.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.rozag.redux.base.ReduxSubscribableStore
+import com.ict.mito.justodo.App
 import com.ict.mito.justodo.R
+import com.ict.mito.justodo.ToDoStore
+import com.ict.mito.justodo.state.ToDoState
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ReduxSubscribableStore.Subscriber<ToDoState> {
+    private val store: ToDoStore = App.store
+    private lateinit var subscription: ReduxSubscribableStore.Subscription
+
+    override fun onNewState(state: ToDoState) {
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +27,15 @@ class MainActivity : AppCompatActivity() {
                     )
                     .commitNow()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        subscription = store.subscribe(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        subscription.cancel()
     }
 }
