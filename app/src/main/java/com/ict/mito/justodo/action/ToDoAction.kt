bed47b1e428262623395eb60d04c4ebd45f1639c
mitohato <mitohato14@gmail.com>
2018-07-29 21:01:36 +0900

    Add args to Action

--- app/src/main/java/com/ict/mito/justodo/action/ToDoAction.kt
+++ app/src/main/java/com/ict/mito/justodo/action/ToDoAction.kt
@@ -1,14 +1,18 @@
 package com.ict.mito.justodo.action
 
 import com.github.rozag.redux.core.ReduxAction
+import com.ict.mito.justodo.model.ToDoInfo
 
 /**
  * Created by mito on 2018/07/14.
  */
 sealed class ToDoAction : ReduxAction {
-    class AddToDo : ToDoAction()
-    class EditToDo : ToDoAction()
-    class DeleteToDo : ToDoAction()
-    class CompleteToDo : ToDoAction()
-    class UnCompleteToDo : ToDoAction()
+    class AddToDo(val toDoInfo: ToDoInfo) : ToDoAction()
+    class UpdateToDo(
+            val taskId: String,
+            val toDoInfo: ToDoInfo
+    ) : ToDoAction()
+    class DeleteToDo(val taskId: String) : ToDoAction()
+    class CompleteToDo(val taskId: String) : ToDoAction()
+    class UnCompleteToDo(val taskId: String) : ToDoAction()
 }
