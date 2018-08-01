fb7b508c083574bb919f6970896eb5d4967a483a
mitohato <mitohato14@gmail.com>
2018-08-01 13:58:56 +0900

    Add default value

--- app/src/main/java/com/ict/mito/justodo/model/ToDoInfo.kt
+++ app/src/main/java/com/ict/mito/justodo/model/ToDoInfo.kt
@@ -1,10 +1,10 @@
 package com.ict.mito.justodo.model
 
 data class ToDoInfo(
-    val id: String,
-    val title: String,
-    val description: String,
-    val dueData: String,
-    val deadline: String,
-    val completed: Boolean
+    val id: String = "",
+    val title: String = "",
+    val description: String = "",
+    val dueData: String = "",
+    val deadline: String = "",
+    val completed: Boolean = false
 )
