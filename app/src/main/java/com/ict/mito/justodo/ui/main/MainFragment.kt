57c6ac3fd73465bd035feb04cea9750aaacd31d0
mitohato <mitohato14@gmail.com>
2018-09-04 07:13:47 +0900

    Remove view value

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
@@ -6,10 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.ict.mito.justodo.R
 import com.ict.mito.justodo.databinding.MainFragmentBinding
-import com.ict.mito.justodo.ui.add.AddActivity
 
 class MainFragment : Fragment() {
 
@@ -21,21 +19,15 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val view = inflater.inflate(
-                R.layout.main_fragment,
-                container,
-                false
-        )
+    ): View? {
+        val binding: MainFragmentBinding? =
+                DataBindingUtil.inflate(
+                        inflater,
+                        R.layout.main_fragment,
+                        container,
+                        false
+                )
 
-        val binding: MainFragmentBinding? = DataBindingUtil.bind(view)
-        binding?.let { it ->
-            it.layoutManager = LinearLayoutManager(this.context?.applicationContext)
-            it.setAddOnClick {
-                AddActivity.start(this@MainFragment.context)
-            }
-        }
-
-        return view
+        return binding?.root
     }
 }
