0def98b07625d7acf1a60c8934f5729eece12e63
mitohato <mitohato14@gmail.com>
2018-09-04 07:08:52 +0900

    Add viewmodel

---
+++ app/src/main/java/com/ict/mito/justodo/ui/add/AddViewModel.kt
@@ -0,0 +1,9 @@
+package com.ict.mito.justodo.ui.add
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by mito on 2018/09/04.
+ */
+class AddViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.ict.mito.justodo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by mito on 2018/09/04.
+ */
+class MainViewModel : ViewModel() {
+}
