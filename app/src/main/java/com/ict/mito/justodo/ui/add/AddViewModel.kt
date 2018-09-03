4a48a86da17521a84c2baf963308214ed8ebe5f0
mitohato <mitohato14@gmail.com>
2018-09-04 08:18:10 +0900

    Add LiveData of todoinfo in AddFragment

--- app/src/main/java/com/ict/mito/justodo/ui/add/AddViewModel.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/add/AddViewModel.kt
@@ -1,8 +1,19 @@
 package com.ict.mito.justodo.ui.add
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ict.mito.justodo.model.ToDoInfo
 
 /**
  * Created by mito on 2018/09/04.
  */
-class AddViewModel : ViewModel()
+class AddViewModel : ViewModel() {
+    var toDoInfo: LiveData<ToDoInfo>? = null
+        get() {
+            if (field == null) {
+                field = MutableLiveData<ToDoInfo>()
+            }
+            return field
+        }
+}
