e8ea9a8700773068c2932703dc1531630d227470
mitohato <mitohato14@gmail.com>
2018-09-04 08:15:40 +0900

    Add LiveData of todos in MainViewModel

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainViewModel.kt
@@ -1,8 +1,19 @@
 package com.ict.mito.justodo.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ict.mito.justodo.model.ToDoInfo
 
 /**
  * Created by mito on 2018/09/04.
  */
-class MainViewModel : ViewModel()
+class MainViewModel : ViewModel() {
+    var todos: LiveData<List<ToDoInfo>>? = null
+    get() {
+        if (field == null) {
+            field = MutableLiveData<List<ToDoInfo>>()
+        }
+        return field
+    }
+}
