190967d93f7db4d02c1cb9c40aeb4b0e95bfd9f4
mitohato <mitohato14@gmail.com>
2018-09-04 07:15:57 +0900

    Add viewmodel in mainactivity

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.ict.mito.justodo.R
 import com.ict.mito.justodo.databinding.MainFragmentBinding
 
@@ -14,6 +15,8 @@ class MainFragment : Fragment() {
     companion object {
         fun newInstance() = MainFragment()
     }
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,5 +32,10 @@ class MainFragment : Fragment() {
                 )
 
         return binding?.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 }
