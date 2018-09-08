da4c5b3d5ebe52ebfbcb8eaaa968d910ce8c6cee
mitohato <mitohato14@gmail.com>
2018-09-08 22:33:51 +0900

    Add support navigation up method

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainActivity.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ict.mito.justodo.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.ict.mito.justodo.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,4 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
     }
+    
+    override fun onSupportNavigateUp() =
+            findNavController(R.id.navhost).navigateUp()
 }
