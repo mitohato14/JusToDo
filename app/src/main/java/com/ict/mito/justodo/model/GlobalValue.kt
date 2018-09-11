7d5c40d8e2fb8eede3e2cf16ccd03fefb5d618f6
mitohato <mitohato14@gmail.com>
2018-09-11 23:29:11 +0900

    Change global list type

--- app/src/main/java/com/ict/mito/justodo/model/GlobalValue.kt
+++ app/src/main/java/com/ict/mito/justodo/model/GlobalValue.kt
@@ -4,5 +4,5 @@ package com.ict.mito.justodo.model
  * Created by mito on 2018/09/10.
  */
 object GlobalValue {
-    val toDoInfoList: List<ToDoInfo> = arrayListOf()
+    val toDoInfoList: ArrayList<ToDoInfo> = arrayListOf()
 }
