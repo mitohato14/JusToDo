6e346a82f2808fb8613626e058b72230293863ab
mitohato <mitohato14@gmail.com>
2018-09-12 07:58:35 +0900

    Rename todoInfo in AddViewModel

--- app/src/main/java/com/ict/mito/justodo/ui/add/AddViewModel.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/add/AddViewModel.kt
@@ -9,7 +9,7 @@ import com.ict.mito.justodo.model.ToDoInfo
  * Created by mito on 2018/09/04.
  */
 class AddViewModel : ViewModel() {
-    var toDoInfo: LiveData<ToDoInfo>? = null
+    var todoInfo: LiveData<ToDoInfo>? = null
         get() {
             if (field == null) {
                 field = MutableLiveData<ToDoInfo>()
