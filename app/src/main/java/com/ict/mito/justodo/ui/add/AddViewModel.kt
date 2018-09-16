1d023f888546718f22c56f3df33730a5a7401f6e
mitohato <mitohato14@gmail.com>
2018-09-16 21:11:29 +0900

    CHange livedata type

--- app/src/main/java/com/ict/mito/justodo/ui/add/AddViewModel.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/add/AddViewModel.kt
@@ -1,6 +1,5 @@
 package com.ict.mito.justodo.ui.add
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ict.mito.justodo.model.ToDoInfo
@@ -9,10 +8,10 @@ import com.ict.mito.justodo.model.ToDoInfo
  * Created by mito on 2018/09/04.
  */
 class AddViewModel : ViewModel() {
-    var todoInfoLiveData: LiveData<ToDoInfo>? = null
+    var todoInfoLiveData: MutableLiveData<ToDoInfo>? = null
         get() {
             if (field == null) {
-                field = MutableLiveData<ToDoInfo>()
+                field = MutableLiveData()
             }
             return field
         }
