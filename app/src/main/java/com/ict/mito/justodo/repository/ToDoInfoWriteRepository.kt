bbf6778e4a41fb8c79f3cf45b9735b4a5d711b9b
mitohato <mitohato14@gmail.com>
2018-09-17 15:56:18 +0900

    Add repository interface for write

---
+++ app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoWriteRepository.kt
@@ -0,0 +1,12 @@
+package com.ict.mito.justodo.repository
+
+/**
+ * Created by mito on 2018/09/17.
+ */
+interface ToDoInfoWriteRepository {
+    fun saveAll()
+
+    fun add()
+
+    fun remove()
+}
