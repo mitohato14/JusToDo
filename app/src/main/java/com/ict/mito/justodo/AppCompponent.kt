d07297149a796be5072d54c11c618573cbbe33ec
mitohato <mitohato14@gmail.com>
2018-09-18 22:38:24 +0900

    AppCompponentの追加

---
+++ app/src/main/java/com/ict/mito/justodo/AppCompponent.kt
@@ -0,0 +1,27 @@
+package com.ict.mito.justodo
+
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import javax.inject.Singleton
+
+/**
+ * Created by mito on 2018/09/18.
+ */
+@Singleton
+@Component(
+        modules = [
+            AndroidInjectionModule::class,
+            module::class
+        ]
+)
+interface AppCompponent {
+    @Component.Builder
+    interface Builder {
+        
+        @BindsInstance
+        fun application(application: App)
+        fun build(): AppCompponent
+    }
+    fun inject(app: App)
+}
