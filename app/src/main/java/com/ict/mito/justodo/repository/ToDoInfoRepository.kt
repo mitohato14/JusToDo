6bf789bd60a935e26d49cbb00feb0808f2cbf5f4
mitohato <mitohato14@gmail.com>
2018-09-18 21:38:49 +0900

    Move logic to RepositoryImpl

--- app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoRepository.kt
+++ app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoRepository.kt
@@ -1,38 +1,6 @@
 package com.ict.mito.justodo.repository
 
-import com.ict.mito.justodo.model.ToDoInfo
-
 /**
  * Created by mito on 2018/09/17.
  */
-class ToDoInfoRepository : ToDoInfoReadRepository, ToDoInfoWriteRepository {
-    private var todos: ArrayList<ToDoInfo> = arrayListOf()
-
-    override fun getAll(): List<ToDoInfo> {
-        return todos
-    }
-
-    override fun getById(id: String): ToDoInfo? {
-        return todos.find { it.id == id }
-    }
-
-    override fun saveAll(todos: ArrayList<ToDoInfo>) {
-        this.todos = todos
-    }
-
-    override fun add(toDoInfo: ToDoInfo) {
-        todos.add(toDoInfo)
-    }
-
-    override fun remove(toDoInfo: ToDoInfo) {
-        todos.remove(toDoInfo)
-    }
-
-    override fun remove(id: String) {
-        todos.forEach {
-            if (it.id == id) {
-                todos.remove(it)
-            }
-        }
-    }
-}
+interface ToDoInfoRepository : ToDoInfoReadRepository, ToDoInfoWriteRepository
---
+++ app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.ict.mito.justodo.repository
+
+import com.ict.mito.justodo.model.ToDoInfo
+
+/**
+ * Created by mito on 2018/09/18.
+ */
+class ToDoInfoRepositoryImpl : ToDoInfoRepository {
+    private var todos: ArrayList<ToDoInfo> = arrayListOf()
+
+    override fun getAll(): List<ToDoInfo> {
+        return todos
+    }
+
+    override fun getById(id: String): ToDoInfo? {
+        return todos.find { it.id == id }
+    }
+
+    override fun saveAll(todos: ArrayList<ToDoInfo>) {
+        this.todos = todos
+    }
+
+    override fun add(toDoInfo: ToDoInfo) {
+        todos.add(toDoInfo)
+    }
+
+    override fun remove(toDoInfo: ToDoInfo) {
+        todos.remove(toDoInfo)
+    }
+
+    override fun remove(id: String) {
+        todos.forEach {
+            if (it.id == id) {
+                todos.remove(it)
+            }
+        }
+    }
+}
