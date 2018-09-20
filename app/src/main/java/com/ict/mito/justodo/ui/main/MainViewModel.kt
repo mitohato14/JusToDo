897122b28b51c53ab9e6ad7b02caae0f8f5efc91
mitohato <mitohato14@gmail.com>
2018-09-20 21:47:18 +0900

    Add MainViewModelFactory

--- app/src/main/java/com/ict/mito/justodo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainViewModel.kt
@@ -5,14 +5,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ict.mito.justodo.model.ToDoInfo
 import com.ict.mito.justodo.repository.ToDoInfoRepository
-import javax.inject.Inject
 
 /**
  * Created by mito on 2018/09/04.
  */
-class MainViewModel @Inject constructor(
-    repository: ToDoInfoRepository
-) : ViewModel() {
+class MainViewModel (repository: ToDoInfoRepository) : ViewModel() {
     var todos: LiveData<List<ToDoInfo>>? = null
     get() {
         if (field == null) {
---
+++ app/src/main/java/com/ict/mito/justodo/ui/main/MainViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.ict.mito.justodo.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ict.mito.justodo.repository.ToDoInfoRepository
+import javax.inject.Inject
+
+/**
+ * Created by mito on 2018/09/20.
+ */
+class MainViewModelFactory @Inject constructor(
+        private val repository: ToDoInfoRepository
+) : ViewModelProvider.NewInstanceFactory() {
+    
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(repository)
+    }
+}
