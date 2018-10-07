c1c4e7637c64dea7fbc7a7f19e963eb748645f01
mitohato <mitohato14@gmail.com>
2018-10-07 21:27:20 +0900

    Change type repository

--- app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoReadRepository.kt
+++ app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoReadRepository.kt
@@ -1,12 +1,14 @@
 package com.ict.mito.justodo.repository
 
 import com.ict.mito.justodo.model.ToDoInfo
+import io.reactivex.Maybe
+import io.reactivex.Single
 
 /**
  * Created by mito on 2018/09/17.
  */
 interface ToDoInfoReadRepository {
-    fun getAll(): List<ToDoInfo>
+    fun getAll(): Single<List<ToDoInfo>>
 
-    fun getById(id: String): ToDoInfo?
+    fun getById(id: String): Maybe<ToDoInfo?>
 }
--- app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoWriteRepository.kt
+++ app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoWriteRepository.kt
@@ -1,16 +1,17 @@
 package com.ict.mito.justodo.repository
 
 import com.ict.mito.justodo.model.ToDoInfo
+import io.reactivex.Completable
 
 /**
  * Created by mito on 2018/09/17.
  */
 interface ToDoInfoWriteRepository {
-    fun saveAll(todos: ArrayList<ToDoInfo>)
+    fun saveAll(todos: ArrayList<ToDoInfo>): Completable
 
-    fun add(toDoInfo: ToDoInfo)
+    fun add(toDoInfo: ToDoInfo): Completable
 
-    fun remove(toDoInfo: ToDoInfo)
+    fun remove(toDoInfo: ToDoInfo): Completable
 
-    fun remove(id: String)
+    fun remove(id: String): Completable
 }
