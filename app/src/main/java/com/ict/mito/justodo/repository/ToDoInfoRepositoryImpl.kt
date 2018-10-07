37776b163b58f9c6500f113b4ffdfa79db6e2077
mitohato <mitohato14@gmail.com>
2018-10-07 21:35:54 +0900

    Fix error

--- app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoRepositoryImpl.kt
+++ app/src/main/java/com/ict/mito/justodo/repository/ToDoInfoRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package com.ict.mito.justodo.repository
 
 import com.ict.mito.justodo.model.ToDoInfo
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Single
 
 /**
  * Created by mito on 2018/09/18.
@@ -8,31 +11,28 @@ import com.ict.mito.justodo.model.ToDoInfo
 class ToDoInfoRepositoryImpl : ToDoInfoRepository {
     private var todos: ArrayList<ToDoInfo> = arrayListOf()
 
-    override fun getAll(): List<ToDoInfo> {
-        return todos
+    override fun getAll(): Single<List<ToDoInfo>> {
+        TODO("Must implementation")
     }
 
-    override fun getById(id: String): ToDoInfo? {
-        return todos.find { it.id == id }
+    override fun getById(id: String): Maybe<ToDoInfo?> {
+//        todos.find { it.id == id }
+        TODO("Must implementation")
     }
 
-    override fun saveAll(todos: ArrayList<ToDoInfo>) {
-        this.todos = todos
+    override fun saveAll(todos: ArrayList<ToDoInfo>): Completable{
+        TODO("Must implementation")
     }
 
-    override fun add(toDoInfo: ToDoInfo) {
-        todos.add(toDoInfo)
+    override fun add(toDoInfo: ToDoInfo): Completable {
+        TODO("Must implementation")
     }
 
-    override fun remove(toDoInfo: ToDoInfo) {
-        todos.remove(toDoInfo)
+    override fun remove(toDoInfo: ToDoInfo): Completable {
+        TODO("Must implementation")
     }
 
-    override fun remove(id: String) {
-        todos.forEach {
-            if (it.id == id) {
-                todos.remove(it)
-            }
-        }
+    override fun remove(id: String): Completable {
+        TODO("Must implementation")
     }
 }
