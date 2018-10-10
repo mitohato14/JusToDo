ed77ca4342d03e806f9479ed48c6dbe5eef4bc11
mitohato <mitohato14@gmail.com>
2018-10-11 06:26:14 +0900

    Fix build error

--- app/src/main/java/com/ict/mito/justodo/model/ToDoId.kt
+++ app/src/main/java/com/ict/mito/justodo/model/ToDoId.kt
@@ -1,6 +1,8 @@
 package com.ict.mito.justodo.model
 
+import com.ict.mito.justodo.utils.Identifier
+
 /**
  * Created by mito on 2018/10/02.
  */
-class ToDoId(value: String) : Identifer<String>(value)
+class ToDoId(value: String) : Identifier<String>(value)
