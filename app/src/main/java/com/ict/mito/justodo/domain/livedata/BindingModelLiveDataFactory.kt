a6e5d5ba60dc7bdbd1270692b652787fccf69e05
mitohato <mitohato14@gmail.com>
2018-10-12 21:21:37 +0900

    Add livedate for bindingmodel

---
+++ app/src/main/java/com/ict/mito/justodo/domain/livedata/BindingModelLiveDataFactory.kt
@@ -0,0 +1,8 @@
+package com.ict.mito.justodo.domain.livedata
+
+/**
+ * Created by mito on 2018/10/12.
+ */
+class BindingModelLiveDataFactory {
+    fun create(): BindingModelLiveData = BindingModelLiveData()
+}
