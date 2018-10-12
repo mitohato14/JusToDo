72aa499f700c60d0efeb87a56c916b7808e2e816
mitohato <mitohato14@gmail.com>
2018-10-12 21:00:31 +0900

    Change remove

--- app/src/main/java/com/ict/mito/justodo/usecase/RemoveToDoUseCase.kt
+++ app/src/main/java/com/ict/mito/justodo/usecase/RemoveToDoUseCase.kt
@@ -6,6 +6,6 @@ import io.reactivex.Completable
 /**
  * Created by mito on 2018/10/02.
  */
-interface EditToDoUseCase {
+interface RemoveToDoUseCase {
     fun execute(id: ToDoId): Completable
 }
--- app/src/main/java/com/ict/mito/justodo/usecase/impl/RemoveToDoUseCaseImpl.kt
+++ app/src/main/java/com/ict/mito/justodo/usecase/impl/RemoveToDoUseCaseImpl.kt
@@ -2,17 +2,15 @@ package com.ict.mito.justodo.usecase.impl
 
 import com.ict.mito.justodo.domain.ToDoId
 import com.ict.mito.justodo.domain.repository.ToDoInfoRepository
-import com.ict.mito.justodo.usecase.EditToDoUseCase
+import com.ict.mito.justodo.usecase.RemoveToDoUseCase
 import io.reactivex.Completable
 import javax.inject.Inject
 
 /**
  * Created by mito on 2018/10/03.
  */
-class EditToDoUseCaseImpl @Inject constructor(
+class RemoveToDoUseCaseImpl @Inject constructor(
     private val repository: ToDoInfoRepository
-) : EditToDoUseCase {
-    override fun execute(id: ToDoId): Completable {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
-    }
+) : RemoveToDoUseCase {
+    override fun execute(id: ToDoId): Completable = repository.remove(id)
 }
