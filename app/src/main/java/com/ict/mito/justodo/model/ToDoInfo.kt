21f66682b5027ce23db8d42a6cf9bedb7519c86f
mitohato <mitohato14@gmail.com>
2018-10-12 15:19:29 +0900

    Remove init value

--- app/src/main/java/com/ict/mito/justodo/model/ToDoInfo.kt
+++ app/src/main/java/com/ict/mito/justodo/model/ToDoInfo.kt
@@ -1,10 +1,10 @@
 package com.ict.mito.justodo.model
 
 data class ToDoInfo(
-    val id: String = "",
-    var title: String = "",
-    var description: String = "",
-    val dueData: String = "",
-    val deadline: String = "",
-    val completed: Boolean = false
+    val id: String,
+    var title: String,
+    var description: String,
+    val dueData: String,
+    val deadline: String,
+    val completed: Boolean
 )
