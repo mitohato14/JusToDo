a897d02bfb51a4fd29f13c452eb267405dc89d42
mitohato <mitohato14@gmail.com>
2018-12-14 09:31:37 +0900

    Fix error

--- app/src/main/java/com/ict/mito/justodo/usecase/impl/UnCompleteToDoUseCaseImpl.kt
+++ app/src/main/java/com/ict/mito/justodo/usecase/impl/UnCompleteToDoUseCaseImpl.kt
@@ -1,10 +1,9 @@
 package com.ict.mito.justodo.usecase.impl
 
 import com.ict.mito.justodo.domain.ToDoId
-import com.ict.mito.justodo.domain.ToDoInfo
 import com.ict.mito.justodo.domain.repository.ToDoInfoRepository
 import com.ict.mito.justodo.usecase.UnCompleteToDoUseCase
-import io.reactivex.Single
+import io.reactivex.Completable
 import javax.inject.Inject
 
 /**
@@ -13,7 +12,7 @@ import javax.inject.Inject
 class UnCompleteToDoUseCaseImpl @Inject constructor(
         repository: ToDoInfoRepository
 ) : UnCompleteToDoUseCase {
-    override fun execute(id: ToDoId): Single<ToDoInfo> {
+    override fun execute(id: ToDoId): Completable {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
