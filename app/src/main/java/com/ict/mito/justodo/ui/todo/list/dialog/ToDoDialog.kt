4414f8fc46bfbf5b558c1ae3b7155a3541fa4d15
mitohato <mitohato14@gmail.com>
2018-12-17 06:47:47 +0900

    Rename dialog fragment

--- app/src/main/java/com/ict/mito/justodo/ui/todo/list/dialog/ToDoDialog.kt
+++
@@ -1,9 +0,0 @@
-package com.ict.mito.justodo.ui.todo.list.dialog
-
-import androidx.fragment.app.DialogFragment
-
-/**
- * Created by mito on 2018-12-15.
- */
-class ToDoDialog : DialogFragment() {
-}
