04383ebec7dc822edc3b779d65022a18c06b1a75
mitohato <mitohato14@gmail.com>
2018-12-18 11:26:55 +0900

    Remake ToDoInfo as Entity

--- app/src/main/java/com/ict/mito/justodo/domain/ToDoInfo.kt
+++ app/src/main/java/com/ict/mito/justodo/domain/ToDoInfo.kt
@@ -1,6 +1,11 @@
 package com.ict.mito.justodo.domain
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class ToDoInfo(
+        @PrimaryKey(autoGenerate = true)
     val id: ToDoId,
     var title: String,
     var description: String,
