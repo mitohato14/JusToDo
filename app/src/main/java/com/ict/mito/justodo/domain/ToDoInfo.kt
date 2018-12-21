160d30c32348e3a4fab3a18219cbae8ab02bfd41
mitohato <mitohato14@gmail.com>
2018-12-21 21:49:24 +0900

    Fix entity

--- app/src/main/java/com/ict/mito/justodo/domain/ToDoInfo.kt
+++ app/src/main/java/com/ict/mito/justodo/domain/ToDoInfo.kt
@@ -1,15 +1,15 @@
 package com.ict.mito.justodo.domain
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "justodo_todo_table")
 data class ToDoInfo(
-    @PrimaryKey(autoGenerate = true)
-    val id: ToDoId,
+    @PrimaryKey(autoGenerate = true) val id: ToDoId,
     var title: String,
     var description: String,
-    val dueData: String,
+    @ColumnInfo(name = "due_date") val dueData: String,
     val deadline: String,
     private var done: Boolean
 ) {
