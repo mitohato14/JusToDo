08cc919889ad93ccad993b35ebd00d714886f4f0
mitohato <mitohato14@gmail.com>
2018-12-25 18:55:07 +0900

    Fix and Add provide method

--- app/src/main/java/com/ict/mito/justodo/domain/db/DataBaseModule.kt
+++ app/src/main/java/com/ict/mito/justodo/domain/db/DataBaseModule.kt
@@ -1,13 +1,26 @@
 package com.ict.mito.justodo.domain.db
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 /**
  * Created by mito on 2018-12-22.
  */
 @Module
-internal class DataBaseModule {
+internal class DataBaseModule(private val context: Context) {
+    @Singleton
     @Provides
-    internal fun provideDataBase() = ToDoRoomDataBase
+    internal fun provideDataBase() = Room.databaseBuilder(
+            context,
+            ToDoRoomDataBase::class.java,
+            "justodo.db"
+    )
+            .allowMainThreadQueries()
+            .build()
+    
+    @Provides
+    internal fun provideDao(dataBase: ToDoRoomDataBase) = dataBase.todoInfoDao()
 }
