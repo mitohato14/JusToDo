9e777eca7e038dc026e9ad865ae357a52d0b47d7
mitohato <mitohato14@gmail.com>
2018-12-28 23:28:46 +0900

    Add coroutine for Room

--- app/src/main/java/com/ict/mito/justodo/ui/todo/list/ToDoListViewModel.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/todo/list/ToDoListViewModel.kt
@@ -1,10 +1,16 @@
 package com.ict.mito.justodo.ui.todo.list
 
-import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ict.mito.justodo.domain.ToDoInfo
 import com.ict.mito.justodo.domain.repository.ToDoInfoRepository
 import io.reactivex.rxkotlin.subscribeBy
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by mito on 2018/09/04.
@@ -12,23 +18,30 @@ import io.reactivex.rxkotlin.subscribeBy
 class ToDoListViewModel(
     private val repository: ToDoInfoRepository
 ) : ViewModel() {
-    val todos: ObservableArrayList<ToDoInfo> = ObservableArrayList()
+    private var parentJob = Job()
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(coroutineContext)
+    
+    var todos: LiveData<List<ToDoInfo>> = MutableLiveData()
 
     init {
         readAll()
     }
 
-    private fun readAll() {
+    private fun readAll() = scope.launch(Dispatchers.IO) {
         repository.getAll().subscribeBy(
                 onSuccess = {
-                    todos.apply {
-                        clear()
-                        addAll(it)
-                    }
+                    todos = MutableLiveData(it)
                 },
                 onError = {
                     todos
                 }
         )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
     }
 }
