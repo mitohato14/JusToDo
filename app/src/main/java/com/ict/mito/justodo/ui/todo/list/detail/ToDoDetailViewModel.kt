a2bb8fa0adfcefba8c969557f9c36bc96dae89b9
mitohato <mitohato14@gmail.com>
2019-02-02 21:51:12 +0900

    Add updateToDo method

--- app/src/main/java/com/ict/mito/justodo/ui/todo/list/detail/ToDoDetailViewModel.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/todo/list/detail/ToDoDetailViewModel.kt
@@ -6,7 +6,12 @@ import androidx.lifecycle.ViewModel
 import com.ict.mito.justodo.domain.ToDoInfo
 import com.ict.mito.justodo.domain.repository.ToDoInfoRepository
 import io.reactivex.rxkotlin.subscribeBy
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import java.sql.Date
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by mitohato14 on 2019-01-23.
@@ -14,6 +19,11 @@ import java.sql.Date
 class ToDoDetailViewModel(
     private val repository: ToDoInfoRepository
 ) : ViewModel() {
+    private var parentJob = Job()
+    private val mainCoroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(mainCoroutineContext)
+
     var todo: LiveData<ToDoInfo> = MutableLiveData()
     var dateString: String = ""
 
@@ -32,5 +42,9 @@ class ToDoDetailViewModel(
                     }
             )
             field = value
+    }
+    
+    fun updateToDo() = scope.launch(Dispatchers.IO) {
+        todo.value?.let { repository.store(it) }
     }
 }
