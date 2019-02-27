6f8dccc9786b08343d3953edeb0efc0493cc2e22
mitohato <mitohato14@gmail.com>
2019-02-27 11:48:12 +0900

    Add done and undone toast

--- app/src/main/java/com/ict/mito/justodo/ui/todo/detail/ToDoDetailFragment.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/todo/detail/ToDoDetailFragment.kt
@@ -8,6 +8,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -83,8 +84,18 @@ class ToDoDetailFragment : Fragment() {
             R.id.action_done -> {
                 if (viewmodel.todo.value?.done == true) {
                     viewmodel.undone()
+                    Toast.makeText(
+                        context,
+                        "undone",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 } else {
                     viewmodel.done()
+                    Toast.makeText(
+                        context,
+                        "done",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
