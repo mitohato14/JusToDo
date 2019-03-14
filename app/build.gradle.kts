f2b94e0d4edac3d1bb3ac0e1b3498e3dcf81da18
mitohato <mitohato14@gmail.com>
2019-03-14 16:39:54 +0900

    Migrate Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,105 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.ict.mito.justodo"
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    dataBinding.isEnabled = true
+}
+
+dependencies {
+    implementation(fileTree(mapOf(
+        "dir" to "libs",
+        "include" to listOf("*.jar")
+    )))
+
+    val kotlinVersion: String? by project
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+
+//    androidx
+    implementation("androidx.appcompat:appcompat:1.1.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.1.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.1.0-alpha02")
+    implementation("com.android.support:multidex:1.0.3")
+//    navigation
+    val navigationVersion: String? by project
+    implementation("android.arch.navigation:navigation-common-ktx:$navigationVersion")
+    implementation("android.arch.navigation:navigation-fragment:$navigationVersion")
+    implementation("android.arch.navigation:navigation-ui:$navigationVersion")
+    implementation("android.arch.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("android.arch.navigation:navigation-ui-ktx:$navigationVersion")
+//    androidx view
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0-alpha3")
+    implementation("androidx.recyclerview:recyclerview:1.1.0-alpha02")
+    implementation("androidx.cardview:cardview:1.0.0")
+//    material view
+    implementation("com.google.android.material:material:1.1.0-alpha03")
+//    room
+    val roomVersion: String? by project
+    implementation("android.arch.persistence.room:runtime:$roomVersion")
+    implementation("android.arch.persistence.room:rxjava2:1.1.1")
+    kapt("android.arch.persistence.room:compiler:$roomVersion")
+    androidTestImplementation("android.arch.persistence.room:testing:$roomVersion")
+//    Lifecycle components
+    val archLifecycleVerison: String? by project
+    implementation("android.arch.lifecycle:extensions:$archLifecycleVerison")
+    kapt("android.arch.lifecycle:compiler:$archLifecycleVerison")
+//    Coroutines
+    val coroutinesVersion: String? by project
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+//    Anko
+    implementation("org.jetbrains.anko:anko-commons:0.10.5")
+
+//    Rx
+    implementation("io.reactivex.rxjava2:rxjava:2.1.13")
+    implementation("io.reactivex.rxjava2:rxandroid:2.0.2")
+    implementation("io.reactivex.rxjava2:rxkotlin:2.2.0")
+
+//    LazyDatePicker
+    implementation("com.mikhaellopez:lazydatepicker:1.0.0")
+
+//    Dagger 2
+    val daggerVersion: String? by project
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    implementation("com.google.dagger:dagger-android:$daggerVersion")
+    implementation("com.google.dagger:dagger-android-support:$daggerVersion")
+    annotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+    kapt("com.google.dagger:dagger-android-processor:$daggerVersion")
+
+//    test
+    testImplementation("junit:junit:4.13-beta-1")
+    androidTestImplementation("androidx.test:runner:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+}
+
+kapt {
+    javacOptions {
+        option("-Xmaxerrs", 5000)
+    }
+}
+kotlin {
+    experimental.coroutines = Coroutines.ENABLE
+}
---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    val kotlinVersion by extra("1.3.21")
+    val daggerVersion by extra("2.16")
+    val navigationVersion by extra("1.0.0-beta01")
+    val roomVersion by extra("1.1.1")
+    val archLifecycleVersion by extra("1.1.1")
+    val coroutinesVersion by extra("0.26.1")
+
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.5.0-alpha07")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven("https://jitpack.io")
+    }
+}
+tasks.create(
+    "clean",
+    type = Delete::class
+) {
+    delete(rootProject.buildDir)
+}
