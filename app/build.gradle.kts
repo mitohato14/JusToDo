e98091f8c07b468256f76996f48b8c328e29fe8f
mitohato <mitohato14@gmail.com>
2019-06-08 22:08:32 +0900

    Update library version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
 
 //    androidx
-    implementation("androidx.appcompat:appcompat:1.1.0-alpha05")
+    implementation("androidx.appcompat:appcompat:1.1.0-beta01")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0-alpha01")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0-alpha01")
     implementation("com.android.support:multidex:1.0.3")
@@ -53,10 +53,10 @@ dependencies {
     implementation("android.arch.navigation:navigation-ui-ktx:$navigationVersion")
 //    androidx view
     implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta1")
-    implementation("androidx.recyclerview:recyclerview:1.1.0-alpha05")
+    implementation("androidx.recyclerview:recyclerview:1.1.0-alpha06")
     implementation("androidx.cardview:cardview:1.0.0")
 //    material view
-    implementation("com.google.android.material:material:1.1.0-alpha06")
+    implementation("com.google.android.material:material:1.1.0-alpha07")
 //    room
     val roomVersion: String? by project
     implementation("android.arch.persistence.room:runtime:$roomVersion")
@@ -95,7 +95,7 @@ dependencies {
 //    test
     testImplementation("junit:junit:4.13-beta-1")
     androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
 
 kapt {
