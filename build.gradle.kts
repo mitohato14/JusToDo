615fc4517b0e9bf6e6fbc14b664106d1597b696c
mitohato <mitohato14@gmail.com>
2019-08-08 20:20:40 +0900

    AS 3.6 canary5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.nio.file.Files.delete
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
@@ -13,7 +15,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.0-alpha03")
+        classpath("com.android.tools.build:gradle:3.6.0-alpha05")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
 
