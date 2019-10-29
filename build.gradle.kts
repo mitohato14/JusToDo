b4565e0eb71502d875697adfe0d35bee7717bfd0
mitohato <mitohato14@gmail.com>
2019-10-29 21:33:12 +0900

    Change gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.0-alpha12")
+        classpath("com.android.tools.build:gradle:4.0.0-alpha01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
 
