bcfcf113b65ef1e8a3b8190261e076d1dd046e2b
mitohato <mitohato14@gmail.com>
2019-10-30 13:28:00 +0900

    Kotlin 1.3.50

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by extra("1.3.31")
+    val kotlinVersion by extra("1.3.50")
     val daggerVersion by extra("2.16")
     val navigationVersion by extra("1.0.0-beta01")
     val roomVersion by extra("1.1.1")
