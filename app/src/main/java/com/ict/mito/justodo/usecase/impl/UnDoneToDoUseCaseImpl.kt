97e1db993f55cb763cfd9e92678ee88a8e3de92e
mitohato <mitohato14@gmail.com>
2020-03-14 09:59:58 +0900

    Add coroutine var

--- app/src/main/java/com/ict/mito/justodo/usecase/impl/UnDoneToDoUseCaseImpl.kt
+++ app/src/main/java/com/ict/mito/justodo/usecase/impl/UnDoneToDoUseCaseImpl.kt
@@ -4,7 +4,11 @@ import com.ict.mito.justodo.domain.ToDoId
 import com.ict.mito.justodo.domain.repository.ToDoInfoRepository
 import com.ict.mito.justodo.usecase.UnDoneToDoUseCase
 import io.reactivex.Completable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by mito on 2018-12-14.
@@ -12,6 +16,10 @@ import javax.inject.Inject
 class UnDoneToDoUseCaseImpl @Inject constructor(
     repository: ToDoInfoRepository
 ) : UnDoneToDoUseCase {
+    private var parentJob = Job()
+    private val mainCoroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.IO
+    private val scope = CoroutineScope(mainCoroutineContext)
     override fun execute(id: ToDoId): Completable {
         TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
