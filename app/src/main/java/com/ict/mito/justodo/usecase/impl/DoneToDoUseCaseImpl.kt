b7166ccbe0e2d4ea944503e2d28b0421d705a3b5
mitohato <mitohato14@gmail.com>
2020-03-16 18:17:38 +0900

    Add new line to end of file

--- app/src/main/java/com/ict/mito/justodo/usecase/impl/DoneToDoUseCaseImpl.kt
+++ app/src/main/java/com/ict/mito/justodo/usecase/impl/DoneToDoUseCaseImpl.kt
@@ -4,9 +4,11 @@ import com.ict.mito.justodo.domain.ToDoId
 import com.ict.mito.justodo.domain.repository.ToDoInfoRepository
 import com.ict.mito.justodo.usecase.DoneToDoUseCase
 import io.reactivex.Completable
+import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
@@ -22,6 +24,17 @@ class DoneToDoUseCaseImpl @Inject constructor(
     private val scope = CoroutineScope(mainCoroutineContext)
 
     override fun execute(id: ToDoId): Completable {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+        return Completable.create { emitter ->
+            repository.getById(id.value.toLong()).subscribeBy(
+                onSuccess = {
+                    scope.launch {
+                        repository.done(it)
+                        emitter.onComplete()
+                    }
+                }, onError = {
+                    emitter.onError(it)
+                }
+            )
+        }
     }
 }
