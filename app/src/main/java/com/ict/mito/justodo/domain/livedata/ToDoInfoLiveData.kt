e64efc896b92454ad412d4ab1369209a05b4fefb
mitohato <mitohato14@gmail.com>
2020-04-25 22:06:43 +0900

    Change type

--- app/src/main/java/com/ict/mito/justodo/domain/livedata/ToDoInfoLiveData.kt
+++ app/src/main/java/com/ict/mito/justodo/domain/livedata/ToDoInfoLiveData.kt
@@ -1,12 +1,12 @@
 package com.ict.mito.justodo.domain.livedata
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ict.mito.justodo.domain.ToDoInfo
 
 /**
  * Created by mito on 2018/10/03.
  */
-class ToDoInfoLiveData : LiveData<ToDoInfo>() {
+class ToDoInfoLiveData : MutableLiveData<ToDoInfo>() {
     override fun onActive() {
         super.onActive()
         value = ToDoInfo(
