3da239ae2239a3ad3a8c9a5af966c9436c7ea884
mitohato <mitohato14@gmail.com>
2020-05-05 13:09:37 +0900

    Remove set onclick

--- app/src/main/java/com/ict/mito/justodo/ui/todo/list/view/ToDoListAdapter.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/todo/list/view/ToDoListAdapter.kt
@@ -1,16 +1,12 @@
 package com.ict.mito.justodo.ui.todo.list.view
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import com.ict.mito.justodo.domain.ToDoInfo
 
 class ToDoListAdapter(private var todoInfoList: List<ToDoInfo>) :
     RecyclerView.Adapter<ToDoListViewHolder>() {
-
-    lateinit var navController: NavController
 
     fun setToDoListData(newData: List<ToDoInfo>) {
         todoInfoList = newData
@@ -32,10 +28,6 @@ class ToDoListAdapter(private var todoInfoList: List<ToDoInfo>) :
         holder: ToDoListViewHolder,
         position: Int
     ) {
-        holder.apply {
-            bind(todoInfoList[position])
-            setToRootOnClickListener(View.OnClickListener {
-            })
-        }
+        holder.bind(todoInfoList[position])
     }
 }
