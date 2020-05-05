1e0f248e2fe3ac40e9c4773ef1a2194d8787b097
mitohato <mitohato14@gmail.com>
2020-05-05 13:30:56 +0900

    Get view height and width

--- app/src/main/java/com/ict/mito/justodo/ui/todo/list/view/ToDoListViewHolder.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/todo/list/view/ToDoListViewHolder.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.core.animation.doOnEnd
 import androidx.core.animation.doOnStart
+import androidx.core.view.doOnLayout
+import androidx.core.view.doOnNextLayout
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.ict.mito.justodo.databinding.CardBaseLayoutBinding
@@ -13,6 +15,15 @@ import com.ict.mito.justodo.utils.getValueAnimator
 
 class ToDoListViewHolder(private val binding: CardBaseLayoutBinding) :
     RecyclerView.ViewHolder(binding.root) {
+
+    private var originalHeightWidthPair = Pair(
+        0,
+        0
+    )
+    private var expandedHeightWidthPair = Pair(
+        0,
+        0
+    )
 
     fun bind(toDoInfo: ToDoInfo) {
         binding.todoInfo = toDoInfo
@@ -23,6 +34,25 @@ class ToDoListViewHolder(private val binding: CardBaseLayoutBinding) :
             )
         }
         binding.executePendingBindings()
+        calcSize()
+    }
+
+    private fun calcSize() {
+        binding.root.doOnLayout { view ->
+            originalHeightWidthPair = Pair(
+                view.height,
+                view.width
+            )
+            binding.cardTodoDetail.root.isVisible = true
+            binding.root.doOnNextLayout {
+                expandedHeightWidthPair = Pair(
+                    it.height,
+                    it.width
+                )
+
+                binding.cardTodoDetail.root.post { binding.cardTodoDetail.root.isVisible = false }
+            }
+        }
     }
 
     fun unbind() {
@@ -38,10 +68,14 @@ class ToDoListViewHolder(private val binding: CardBaseLayoutBinding) :
             1000,
             AccelerateDecelerateInterpolator()
         ) { progress ->
-//                holder.binding.root.layoutParams.height =
-//                    (originalHeight + (expandedHeight - originalHeight) * progress).toInt()
-//                holder.binding.root.layoutParams.width =
-//                    (originalWidth + (expandedWidth - originalWidth) * progress).toInt()
+            holder.binding.root.layoutParams.height =
+                (originalHeightWidthPair.first +
+                    (expandedHeightWidthPair.first - originalHeightWidthPair.first)
+                    * progress).toInt()
+            holder.binding.root.layoutParams.width =
+                (originalHeightWidthPair.second +
+                    (expandedHeightWidthPair.second - originalHeightWidthPair.second)
+                    * progress).toInt()
 
             holder.binding.root.requestLayout()
         }
@@ -75,3 +109,5 @@ class ToDoListViewHolder(private val binding: CardBaseLayoutBinding) :
         )
     }
 }
+
+data class ViewSize(val width: Int, val height: Int)
