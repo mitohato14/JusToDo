9ef0480b3e20403c08decf2893e358771371bb46
mitohato <mitohato14@gmail.com>
2020-05-05 13:11:08 +0900

    Switch layout view

--- app/src/main/java/com/ict/mito/justodo/ui/todo/list/view/ToDoListViewHolder.kt
+++ app/src/main/java/com/ict/mito/justodo/ui/todo/list/view/ToDoListViewHolder.kt
@@ -3,15 +3,27 @@ package com.ict.mito.justodo.ui.todo.list.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.ict.mito.justodo.databinding.CardBaseLayoutBinding
 import com.ict.mito.justodo.domain.ToDoInfo
+import com.ict.mito.justodo.utils.getValueAnimator
 
 class ToDoListViewHolder(private val binding: CardBaseLayoutBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(toDoInfo: ToDoInfo) {
         binding.todoInfo = toDoInfo
+        binding.root.setOnClickListener {
+            expandItem(
+                holder = this,
+                expand = true,
+                animate = true
+            )
+        }
         binding.executePendingBindings()
     }
 
@@ -19,8 +31,40 @@ class ToDoListViewHolder(private val binding: CardBaseLayoutBinding) :
         binding.unbind()
     }
 
-    fun setToRootOnClickListener(onClickListener: View.OnClickListener) {
-        binding.root.setOnClickListener(onClickListener)
+    private fun expandItem(
+        holder: ToDoListViewHolder,
+        expand: Boolean,
+        animate: Boolean
+    ) {
+        if (animate) {
+            val animator = getValueAnimator(
+                expand,
+                1000,
+                AccelerateDecelerateInterpolator()
+            ) { progress ->
+//                holder.binding.root.layoutParams.height =
+//                    (originalHeight + (expandedHeight - originalHeight) * progress).toInt()
+//                holder.binding.root.layoutParams.width =
+//                    (originalWidth + (expandedWidth - originalWidth) * progress).toInt()
+
+                holder.binding.root.requestLayout()
+
+            }
+
+            if (expand) {
+                animator.doOnStart {
+                    holder.binding.cardTodoDetail.root.isVisible = true
+                    holder.binding.cardTodo.root.isVisible = false
+                }
+            } else {
+                animator.doOnEnd {
+                    holder.binding.cardTodoDetail.root.isVisible = false
+                    holder.binding.cardTodo.root.isVisible = false
+                }
+            }
+
+            animator.start()
+        }
     }
 
     companion object {
