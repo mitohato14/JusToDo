e69372fe061b82fa0143e6c22e99add42ae9b2ab
mitohato <mitohato14@gmail.com>
2020-10-31 15:48:03 +0900

    Update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
         maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.3")
+        classpath("com.android.tools.build:gradle:4.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:8.2.0")
